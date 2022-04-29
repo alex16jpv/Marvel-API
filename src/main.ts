a404689beab9cec57ac652cb296774cdb72d1b0d
Alexander Valencia <aperez@hellobuild.co>
2022-04-28 23:10:23 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 require('dotenv').config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://marvel.alexpvalencia.com',
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
