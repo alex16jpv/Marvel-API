b659db93cc2c088e6f31c9aaad23915efce1c328
Alexander Valencia <aperez@hellobuild.co>
2022-04-30 21:39:15 -0500

    feat: get cors origin from .env

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://marvel.alexpvalencia.com',
+    origin: process.env.CORS_ORIGIN,
   });
 
   await app.listen(process.env.PORT || 3000);
