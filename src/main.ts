2d1018d7cbf021a7a0c58531cffaafd638cd160b
Alexander Valencia <alex16jpv@gmail.com>
2023-09-24 11:34:02 -0500

    fix: CORS validation

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,18 @@ require('dotenv').config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const ALLOWED_ORIGINS = process.env.CORS_ORIGIN.split(',') || [];
   app.enableCors({
-    origin: process.env.CORS_ORIGIN,
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (ALLOWED_ORIGINS.includes(origin)) {
+        return callback(null, true);
+      }
+      const msg =
+        'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    },
   });
 
   await app.listen(process.env.PORT || 3000);
